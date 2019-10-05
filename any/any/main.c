eeab6c78748674df147fb885e0e1b3f6d0990153
JWMON <danier123@gmail.com>
2019-10-06 02:42:50 +0900

    draft

---
+++ any/any/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  any
+//
+//  Created by JWMON on 05/10/2019.
+//  Copyright © 2019 JWMON. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
